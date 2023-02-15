30b0ff813eb3f109d699539b770e98d6dd19a96c
pulley71 <91060436+pulley71@users.noreply.github.com>
2023-02-15 12:52:40 +0100

    cambiado metas

--- wp-content/themes/asdrubal/components/metas-seo.php
+++ wp-content/themes/asdrubal/components/metas-seo.php
@@ -1,41 +1,47 @@
+<?php
+$term= get_queried_objetct();
+?>
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta charset="UTF-8">
+    
+    <meta name= "robots" content="" >
 
-<meta name= "robots" content="" >
+    <?php the_field( 'custom_meta', $term ); ?>
 
-<meta http-equiv="" content="" />
-<title>Página</title>
-<meta property="og:title" content="">
-<meta property="twitter:title" content="">
+    <meta http-equiv="" content="" />
+    <title></title>
+    <meta property="og:title" content="">
+    <meta property="twitter:title" content="">
 
-<meta name= "description" content="" >
+    <meta name= "description" content="" >
 
-<meta name="robots" content="">
-<meta name="bingbot" content="">
+    <meta name="robots" content="">
+    <meta name="bingbot" content="">
 
 
-<meta property="og:url" content="">
-<meta property="twitter:url" content="">
+    <meta property="og:url" content="">
+    <meta property="twitter:url" content="">
 
-<meta property="og:description" content="">
-<meta property="twitter:description" content="">
+    <meta property="og:description" content="">
+    <meta property="twitter:description" content="">
 
-<meta property="og:image" content="">
-<meta property="twitter:image" content="">
-<meta property="og:image:alt" content="">
+    <meta property="og:image" content="">
+    <meta property="twitter:image" content="">
+    <meta property="og:image:alt" content="">
 
-<meta property="og:type" content="website">
-<meta property="twitter:card" content="sumary_large_image">
-<meta name="twitter:site" content="@andres">
-<meta name="twitter:creator" content="@andres">
-<!--
-<meta name="robots" content="all">
-<meta name="robots" content="index, follow">
-<meta name="robots" content="index, nofollow">
-<meta name="robots" content="noindex, follow">
-<meta name="robots" content="noindex, nofollow">
-<meta name="robots" content="none">
-<meta name="robots" content="index, max-snippet:-1" caracteres que se quiera>
-<meta name="robots" content="index, max-snippet: 0" igual que nosnippet>
--->
-<meta name="rating" content="adult">
+    <meta property="og:type" content="website">
+    <meta property="twitter:card" content="sumary_large_image">
+    <meta name="twitter:site" content="@andres">
+    <meta name="twitter:creator" content="@andres">
+    <!--
+    <meta name="robots" content="all">
+    <meta name="robots" content="index, follow">
+    <meta name="robots" content="index, nofollow">
+    <meta name="robots" content="noindex, follow">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="robots" content="none">
+    <meta name="robots" content="index, max-snippet:-1" caracteres que se quiera>
+    <meta name="robots" content="index, max-snippet: 0" igual que nosnippet>
+    -->
+    <meta name="rating" content="adult">
